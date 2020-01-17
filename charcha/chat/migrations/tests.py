6fa21c7bfdc749aa0e2157d943da9c1e41b5f8ae
iamkreeti <kritisingh12398@gmail.com>
2020-01-17 13:40:38 +0530

    Created an new test.py file in migrations folder

---
+++ charcha/chat/migrations/tests.py
@@ -0,0 +1,48 @@
+from django.test import TestCase
+from charcha.discussions.models import User
+from charcha.team.models import Team, TeamMember, ADMIN, MEMBER
+
+from .models import Channel, Message
+
+class DiscussionTests(TestCase):
+    def setUp(self):
+        self._create_team()
+
+    def _create_team(self):
+        team = Team(name="trial team")
+        team.save()
+        self.team = team
+
+        Ankita = User.objects.create_user(
+            username="Ankita", password="top_secret")
+        Ansh = User.objects.create_user(
+            username="Ansh", password="top_secret")
+        Smriti = User.objects.create_user(
+            username="Smriti", password="top_secret")
+
+        self.Ankita = TeamMember(team=team, user=Ankita, role=ADMIN)
+        self.Ankita.save()
+        self.Ansh = TeamMember(team=team, user=Ansh, role=MEMBER)
+        self.Ansh.save()
+        self.Smriti = TeamMember(team=team, user=Smriti, role=MEMBER)
+        self.Smriti.save()
+
+    def test_private_message(self):
+        Ankita, Ansh, Smriti = self.Ankita, self.Ansh, self.Smriti
+
+        self.assertTrue(not Ankita.get_messages_since(0))
+        self.assertTrue(not Ansh.get_messages_since(0))
+
+        self.Ankita.send_direct_message(self.Ansh, "Hey Ansh!")
+        self.Ankita.send_direct_message(self.Ansh, "How are you today?")
+
+        self.assertEquals(len(Ankita.get_messages_since(0)), 2)
+        self.assertEquals(len(Ansh.get_messages_since(0)), 2)
+        self.assertEquals(len(Smriti.get_messages_since(0)), 0)
+
+        self.Ansh.send_direct_message(self.Ankita, "I am doing good. How about you?")
+
+        self.assertEquals(len(Ankita.get_messages_since(0)), 3)
+        self.assertEquals(len(Ankita.get_messages_since(2)), 1)
+        self.assertEquals(len(Ansh.get_messages_since(2)), 1)
+        self.assertEquals(len(Smriti.get_messages_since(0)), 0)
--- charcha/discussions/tests.py
+++ charcha/discussions/tests.py
@@ -5,14 +5,14 @@ from .models import Post, Vote, Comment, User
 class DiscussionTests(TestCase):
     def setUp(self):
         self._create_users()
-        
+
     def _create_users(self):
-        self.ramesh = User.objects.create_user(
-            username="ramesh", password="top_secret")
-        self.amit = User.objects.create_user(
-            username="amit", password="top_secret")
-        self.swetha = User.objects.create_user(
-            username="swetha", password="top_secret")
+        self.Ankita = User.objects.create_user(
+            username="Ankita", password="top_secret")
+        self.Ansh = User.objects.create_user(
+            username="Ansh", password="top_secret")
+        self.Smriti = User.objects.create_user(
+            username="Smriti", password="top_secret")
         self.anamika = AnonymousUser()
 
     def new_discussion(self, user, title):
@@ -21,27 +21,27 @@ class DiscussionTests(TestCase):
             author=user)
         post.save()
         return post
-    
+
     def test_I_cant_vote_for_me(self):
-        post = self.new_discussion(self.ramesh, "Ramesh's Biography")
+        post = self.new_discussion(self.Ankita, "Ankita's Biography")
         self.assertEquals(post.upvotes, 0)
-        post.upvote(self.ramesh)
+        post.upvote(self.Ankita)
         post = Post.objects.get(pk=post.id)
         self.assertEquals(post.upvotes, 0)
 
     def test_double_voting(self):
-        post = self.new_discussion(self.ramesh, "Ramesh's Biography")
+        post = self.new_discussion(self.Ankita, "Ankita's Biography")
         self.assertEquals(post.upvotes, 0)
-        post.upvote(self.amit)
+        post.upvote(self.Ansh)
         post = Post.objects.get(pk=post.id)
         self.assertEquals(post.upvotes, 1)
-        post.upvote(self.amit)
+        post.upvote(self.Ansh)
         post = Post.objects.get(pk=post.id)
         self.assertEquals(post.upvotes, 1)
 
     def test_voting_on_home_page(self):
-        # Ramesh starts a discussion
-        post = self.new_discussion(self.ramesh, "Ramesh's Biography")
+        # Ankita starts a discussion
+        post = self.new_discussion(self.Ankita, "Ankita's Biography")
 
         # Then Anamika views home page
         posts = Post.objects.recent_posts_with_my_votes()
@@ -50,31 +50,31 @@ class DiscussionTests(TestCase):
         self.assertEquals(post.upvotes, 0)
         self.assertEquals(post.downvotes, 0)
 
-        # Amit upvotes the post
-        post.upvote(self.amit)
+        # Ansh upvotes the post
+        post.upvote(self.Ansh)
 
-        # Home page as seen by Amit
-        post = Post.objects.recent_posts_with_my_votes(self.amit).first()
+        # Home page as seen by Ansh
+        post = Post.objects.recent_posts_with_my_votes(self.Ansh).first()
         self.assertTrue(post.is_upvoted)
         self.assertFalse(post.is_downvoted)
         self.assertEquals(post.upvotes, 1)
         self.assertEquals(post.downvotes, 0)
 
-        # Swetha downvotes
-        post.downvote(self.swetha)
+        # Smriti downvotes
+        post.downvote(self.Smriti)
 
-        # Home page as seen by Swetha
-        post = Post.objects.recent_posts_with_my_votes(self.swetha).first()
+        # Home page as seen by Smriti
+        post = Post.objects.recent_posts_with_my_votes(self.Smriti).first()
         self.assertFalse(post.is_upvoted)
         self.assertTrue(post.is_downvoted)
         self.assertEquals(post.upvotes, 1)
         self.assertEquals(post.downvotes, 1)
-        
-        # Amit undo's his vote
-        post.undo_vote(self.amit)
 
-        # Home page as seen by Amit
-        post = Post.objects.recent_posts_with_my_votes(self.amit).first()
+        # Ansh undo's his vote
+        post.undo_vote(self.Ansh)
+
+        # Home page as seen by Ansh
+        post = Post.objects.recent_posts_with_my_votes(self.Ansh).first()
         self.assertFalse(post.is_upvoted)
         self.assertFalse(post.is_downvoted)
         self.assertEquals(post.upvotes, 0)
@@ -86,16 +86,16 @@ class DiscussionTests(TestCase):
         _c3 = "Why write your biography when you haven't achieved a thing!"
         _c4 = "Seriously, that's all you have to say?"
 
-        post = self.new_discussion(self.ramesh, "Ramesh's Biography")
+        post = self.new_discussion(self.Ankita, "Ankita's Biography")
         self.assertEquals(post.num_comments, 0)
 
-        rameshs_comment = post.add_comment(_c1, self.ramesh)
-        amits_comment = rameshs_comment.reply(_c2, self.amit)
-        swethas_comment = rameshs_comment.reply(_c3, self.swetha)
-        rameshs_response = amits_comment.reply(_c4, self.ramesh)
+        Ankitas_comment = post.add_comment(_c1, self.Ankita)
+        Anshs_comment = Ankitas_comment.reply(_c2, self.Ansh)
+        Smritis_comment = Ankitas_comment.reply(_c3, self.Smriti)
+        Ankitas_response = Anshs_comment.reply(_c4, self.Ankita)
 
-        comments = [c.text for c in 
-                    Comment.objects.best_ones_first(post.id, self.ramesh.id)]
+        comments = [c.text for c in
+                    Comment.objects.best_ones_first(post.id, self.Ankita.id)]
 
         self.assertEquals(comments, [_c1, _c2, _c4, _c3])
 
